9a4e39f509cca3f9395a275ff5351e9655bffe99
Nozomu Miyamoto <irregular0number@gmail.com>
2019-05-16 09:43:35 +0900

    ディレクトリ構造修正

--- install.sh
+++
@@ -1,6 +0,0 @@
-# /bin/bash
-git clone https://github.com/nontan18/rancher-portal.git && cd rancher-portal
-docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  -v $PWD:/code:ro \
-  wernight/docker-compose up
